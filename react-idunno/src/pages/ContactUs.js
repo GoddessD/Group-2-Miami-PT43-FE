3dc62ee0ed286124cfc9182a2c97cc723e4c7612
GoddessD <dlouis314@gmail.com>
2023-01-28 01:10:48 +0000

    updated ct pg

--- react-idunno/src/pages/ContactUs.js
+++ react-idunno/src/pages/ContactUs.js
@@ -4,23 +4,55 @@ import "../App.css"
 
 const ContactUs = () => {
   return (
-    <><div className='container-left'>
-      <div className='Title'><h1>Contact Us</h1></div>
 
-      <div className='Address'><h4><i class="fas-duotone fa-map-location">111 NE 1st Street, Suite 808,Miami, FL 33132</i></h4></div>
-
-      <div className='OfficeNumber'><h4><i class="fas-duotone fa-phone-office"></i>1-888-843-2378 (THE BEST)</h4></div>
-
-      <div className='workemail'><h4><i class="fas-solid fa-envelope"></i>admin@idunno.com</h4></div>
-
-      <div className='Website'><h4><i class="fas-regular fa-globe"></i>https://www.idunno.com/</h4></div>
+    <><div classname="corners">
+      <div classname='top-left-corner'></div>
+      <div classname='bottom-right-corner'></div>
+      <div classname='top-right-corner'></div>
+      <div classname='bottom-right-corner'></div>
     </div>
+    
+    <div className='container-left'>
+        <div classname='contact-container'>
+          <div className='Title'><h1>Contact Us</h1></div>
 
-      <div className="appcover">
-        <img src='https://files.slack.com/files-pri/T0BFXMWMV-F04JGTD7CUX/183bf777-c663-4be3-80bf-d4135583fd0b.png' className='img-fluid rounded' alt='' />
-      </div>
-    </>
+
+          <div className='Address'><h8><i class="fa-solid fa-location-dot"> 111 NE 1st Street,Ste 808,Miami, FL 33132</i></h8></div>
+
+          <div className='OfficeNumber'><h5><i class="fa-solid fa-phone-volume"></i> 1-888-843-2378 (THE BEST)</h5></div>
+
+          <div className='workemail'><h5><i class="fa-solid fa-envelope"></i> admin@idunno.com</h5></div>
+          
+          <div className='Website'><h5><i class="fa-sharp fa-solid fa-globe"></i> https://www.idunno.com/</h5></div>
+        </div></div>
+        
+        
+        
+        
+        
+        
+        
+        <div classname="contact form">
+        <form>
+          <div class="name">
+            <label for="InputName">Full Name*</label>
+            <input type="name" class="form-control" id="InputName" placeholder="Name" />
+          </div>
+          <div class="email">
+            <label for="exampleInputEmail1">Email Address*</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          </div>
+          <div class="phone-number">
+            <label for="exampleInputPassword1">Phone Number</label>
+            <input type="phonenumber" class="form-control" id="InputPhoneNumber" placeholder="Phone Number" />
+          </div>
+          <div class="message">
+            <label for="exampleInputPassword1 form-control">Message*</label>
+            <input type="message" class="form-control" id="InputMessage" placeholder="Message" />
+          </div>
+        </form>
+      </div></>
         );
 };
 
-        export default ContactUs;
+       export default ContactUs;
