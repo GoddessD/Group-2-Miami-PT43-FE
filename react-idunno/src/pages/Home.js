e50b57b575f922a37dede2e327452f6e0e9fb694
StaphanieKing <staphaniek@yahoo.com>
2023-02-24 02:05:23 +0000

    sign up log in working

--- react-idunno/src/pages/Home.js
+++ react-idunno/src/pages/Home.js
@@ -19,67 +19,73 @@ const Home = () => {
             <button type="button" class="btn btn-danger">MAKE MY PLANS</button>
           </Link>
         </h1>
+        </div>
 
-      </div>
+        
       <div className='image-wrapper'>
         <img src='/idkicon.png' alt='robot' />
       </div>
       <div class='main-content'>
       </div>
-
-<div class="row">
-  <div class="card mb-3" style={{ maxWidth: "540px" }}>
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src='/spoon and fork.png' class="img-fluid rounded-start" alt="..." />
-      </div>
-      <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">HUNGRY???</h5>
-      <p class="card-text">idunno generates restaurants in your area for you to enjoy dinner weather its with your friends , solo, or </p>
-    </div>
-  </div>
-</div>
-</div>
-
-
-          <div class="card mb-3" style={{ maxWidth: "540px" }}>
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src='/family icon.webp' class="img-fluid rounded-start" alt="..." />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title"> FAMILY OUTING </h5>
-                  <p class="card-text">We know family is everything , allowing us to <br />plan your outing gives you more time to enjoy them. </p>
-
-                </div>
+      
+      
+      
+      
+      
+      <div class="row">
+        <div class="card mb-3" style={{ maxWidth: "540px" }}>
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src='/spoon and fork.png' class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">HUNGRY???</h5>
+                <p class="card-text">idunno generates restaurants in your area for you to enjoy dinner weather its with your friends , solo, or </p>
               </div>
             </div>
           </div>
+        </div>
 
 
-  <div class="card mb-3" style={{ maxWidth: "540px" }}>
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src='/idunno man.png' class="img-fluid rounded-start" alt="..."/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">SOLO PLANS</h5>
-        <p class="card-text">Introvert, selfcare, or maybe just a "YOU" day, Lonely with No Plans ?<br /> We got YOU. </p>
+        <div class="card mb-3" style={{ maxWidth: "540px" }}>
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src='/family icon.webp' class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title"> FAMILY OUTING </h5>
+                <p class="card-text">We know family is everything , allowing us to <br />plan your outing gives you more time to enjoy them. </p>
 
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="card mb-3" style={{ maxWidth: "540px" }}>
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src='/idunno man.png' class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">SOLO PLANS</h5>
+                <p class="card-text">Introvert, selfcare, or maybe just a "YOU" day, Lonely with No Plans ?<br /> We got YOU. </p>
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <img src='/Robothelp.png' alt='robot' />
     </div>
-  </div>
-</div>
-</div>
-        
-<img src='/Robothelp.png' alt='robot' />
-</div>
-  
-);
+
+
+  );
 }
-      
-      
+
+
 export default Home;
